ad194cf31168f9e2a909e9b2de99a10671e88f05
Kolandone <sjamshid81@gmail.com>
2024-07-18 23:35:21 +0330

    Update Sel.sh

--- Sel.sh
+++ Sel.sh
@@ -4,8 +4,8 @@ clear
 echo -e "\e[1;31mY\e[1;32mO\e[1;33mU\e[1;34mT\e[1;35mU\e[1;36mB\e[1;37mE\e[0m : \e[1;31mK\e[1;32mO\e[1;33mL\e[1;34mA\e[1;35mN\e[1;36mD\e[1;37mO\e[1;31mN\e[1;32mE\e[0m"
 
 echo "Please choose an option:"
-echo "1. Run koland.sh"
-echo "2. Run install.sh"
+echo "1. V2ray and MahsaNG wireguard config"
+echo "2. Hiddify config,After the first use, you can enter the \e[1;32mKOLAND command"
 echo "Enter your choice:"
 read -r user_input
 
