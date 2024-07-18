9d67bc69be467170b89cee41fa3c2d71c1331938
Kolandone <sjamshid81@gmail.com>
2024-07-18 23:52:31 +0330

    Update Sel.sh

--- Sel.sh
+++ Sel.sh
@@ -6,8 +6,8 @@ echo -e "\e[1;31mY\e[1;32mO\e[1;33mU\e[1;34mT\e[1;35mU\e[1;36mB\e[1;37mE\e[0m : 
 echo "Please choose an option:"
 echo "1. V2ray and MahsaNG wireguard config"
 echo -e "2. Hiddify config, After the first use, you can enter the \e[1;32mKOLAND\e[0m command"
-echo "Enter your choice:"
-read -r user_input
+echo "Enter your choice:" read -r user_input
+
 
 if [ "$user_input" -eq 1 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/koland.sh)
