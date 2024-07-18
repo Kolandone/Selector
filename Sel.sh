de3c5ced21d1e417bcccf79197f236e2bfd3f239
Kolandone <sjamshid81@gmail.com>
2024-07-18 23:29:58 +0330

    Update Sel.sh

--- Sel.sh
+++ Sel.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+clear
 # Display the title in multiple colors and big size
 echo -e "\e[1;31mY\e[1;32mO\e[1;33mU\e[1;34mT\e[1;35mU\e[1;36mB\e[1;37mE\e[0m : \e[1;31mK\e[1;32mO\e[1;33mL\e[1;34mA\e[1;35mN\e[1;36mD\e[1;37mO\e[1;31mN\e[1;32mE\e[0m"
 
 echo "Please choose an option:"
 echo "1. Run koland.sh"
 echo "2. Run install.sh"
+echo "Enter your choice:"
 read -r user_input
 
 if [ "$user_input" -eq 1 ]; then
