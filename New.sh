35364ca87b28dd547d47ecca1210d4bd820bdb4f
Kolandone <sjamshid81@gmail.com>
2024-07-19 02:21:17 +0330

    Update New.sh

--- New.sh
+++ New.sh
@@ -14,45 +14,51 @@ read -r user_input
 measure_latency() {
     local ip_port=$1
     local ip=$(echo $ip_port | cut -d: -f1)
-    local port=$(echo $ip_port | cut -d: -f2)
-    local latency=$(ping -c 4 -W 1 $ip | tail -1 | awk -F '/' '{print $5}')
-    printf "| %-15s | %-10s | %-10s |\n" "$ip" "$port" "$latency"
+    local latency=$(ping -c 1 -W 1 $ip | grep 'time=' | awk -F'time=' '{ print $2 }' | cut -d' ' -f1)
+    if [ -z "$latency" ]; then
+        latency="N/A"
+    fi
+    printf "| %-21s | %-10s |\n" "$ip_port" "$latency"
 }
 
 measure_latency6() {
     local ip_port=$1
     local ip=$(echo $ip_port | cut -d: -f1)
-    local port=$(echo $ip_port | cut -d: -f2)
-    local latency=$(ping6 -c 4 -W 1 $ip | tail -1 | awk -F '/' '{print $5}')
-    printf "| %-45s | %-10s | %-10s |\n" "$ip" "$port" "$latency"
+    local latency=$(ping6 -c 1 -W 1 $ip | grep 'time=' | awk -F'time=' '{ print $2 }' | cut -d' ' -f1)
+    if [ -z "$latency" ]; then
+        latency="N/A"
+    fi
+    printf "| %-45s | %-10s |\n" "$ip_port" "$latency"
 }
 
-display_ipv4_table() {
-    printf "+-----------------+------------+------------+\n"
-    printf "| IP              | Port       | Ping(ms)   |\n"
-    printf "+-----------------+------------+------------+\n"
-    echo "$1" | while read -r ip_port; do measure_latency "$ip_port"; done
-    printf "+-----------------+------------+------------+\n"
+display_table_ipv4() {
+    printf "+-----------------------+------------+\n"
+    printf "| IP:Port               | Latency(ms) |\n"
+    printf "+-----------------------+------------+\n"
+    echo "$1" | head -n 10 | while read -r ip_port; do measure_latency "$ip_port"; done
+    printf "+-----------------------+------------+\n"
 }
 
-display_ipv6_table() {
-    printf "+-----------------------------------------------+------------+------------+\n"
-    printf "| IP                                            | Port       | Ping(ms)   |\n"
-    printf "+-----------------------------------------------+------------+------------+\n"
-    echo "$1" | while read -r ip_port; do measure_latency6 "$ip_port"; done
-    printf "+-----------------------------------------------+------------+------------+\n"
+display_table_ipv6() {
+    printf "+---------------------------------------------+------------+\n"
+    printf "| IP:Port                                     | Latency(ms) |\n"
+    printf "+---------------------------------------------+------------+\n"
+    echo "$1" | head -n 10 | while read -r ip_port; do measure_latency6 "$ip_port"; done
+    printf "+---------------------------------------------+------------+\n"
 }
 
 if [ "$user_input" -eq 1 ]; then
     echo "Fetching IPv4 addresses from install.sh..."
     ip_list=$(echo "1" | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh) | grep -oP '(\d{1,3}\.){3}\d{1,3}:\d+')
     clear
-    display_ipv4_table "$ip_list"
+    echo "Top 10 IPv4 addresses with their latencies:"
+    display_table_ipv4 "$ip_list"
 elif [ "$user_input" -eq 2 ]; then
     echo "Fetching IPv6 addresses from install.sh..."
     ip_list=$(echo "2" | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh) | grep -oP '(\[?[a-fA-F\d:]+\]?\:\d+)')
     clear
-    display_ipv6_table "$ip_list"
+    echo "Top 10 IPv6 addresses with their latencies:"
+    display_table_ipv6 "$ip_list"
 elif [ "$user_input" -eq 3 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/koland.sh)
 elif [ "$user_input" -eq 4 ]; then
