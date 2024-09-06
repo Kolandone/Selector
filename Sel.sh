b07d90b6f812efc0ace96c93e73c84479cbc028a
KOLAND <sjamshid81@gmail.com>
2024-09-06 04:18:55 +0330

    Update Sel.sh

--- Sel.sh
+++ Sel.sh
@@ -280,8 +280,10 @@ elif [ "$user_input" -eq 8 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/workercreator/main/run.sh)
 elif [ "$user_input" -eq 9 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/quick-sub/main/run.sh)
-elif [ "$user_input" = "install" ]; then
-    bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/Selector/main/install.sh)
 else
+if [ "$user_input" = "install" ]; then
+    bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/Selector/main/install.sh)
+    else 
     echo "Invalid input. Please enter between 1 and 9."
+    fi
 fi
