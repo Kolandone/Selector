d81673ffe5b030aebd3a20a8b88fbcd7eb7b6b1a
KOLAND <sjamshid81@gmail.com>
2024-10-02 13:27:44 +0330

    Update Sel.sh

--- Sel.sh
+++ Sel.sh
@@ -24,6 +24,7 @@ echo -e "\e[1;36m9. \e[1;33m Free subscription link (Hin VPN)\e[0m"
 echo -e "\e[1;36m10. \e[1;33mwireguard config for Hiddify and v2ray\e[0m"
 echo -e "\e[1;36m11. \e[1;33mCLEAN IP scanner\e[0m"
 echo -e "\e[1;36m12. \e[1;33mfastly CLEAN IP scanner\e[0m"
+echo -e "\e[1;36m13. \e[1;33mgcore CLEAN IP scanner\e[0m"
 echo -e "\e[1;36m*****************************************"
 echo -en "\e[1;32mEnter your choice:\e[0m"
 read -r user_input
@@ -289,9 +290,11 @@ elif [ "$user_input" -eq 11 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/cfipscanner/main/ipscan.sh)
 elif [ "$user_input" -eq 12 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/fastlyipscan/refs/heads/main/ipscan.sh)
+elif [ "$user_input" -eq 12 ]; then
+    bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/gcorescanner/refs/heads/main/gcore.sh)
 elif [ "$user_input" -eq 99 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/Selector/main/install.sh)
     echo -e "\e[1;32mAfter this, you can run the Selector with \e[1;36mkl \e[1;32mcommand\e[0m"
     else 
-    echo "Invalid input. Please enter between 1 and 12"
+    echo "Invalid input. Please enter between 1 and 13"
 fi
