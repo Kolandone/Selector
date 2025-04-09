e26eff8c222245d827d1c881ca4f52eeb04adba9
KOLAND <sjamshid81@gmail.com>
2025-04-09 23:45:09 +0330

    Update Sel.sh

--- Sel.sh
+++ Sel.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 clear
 
+# Define color codes
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+purple='\e[1;34m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+reset='\e[0m'
 
-
-echo -e "\e[1;35m*****************************************"
-echo -e "\e[1;35m*\e[0m \e[1;31mY\e[1;32mO\e[1;33mU\e[1;34mT\e[1;35mU\e[1;36mB\e[1;37mE\e[0m : \e[4;34mKOLANDONE\e[0m         \e[1;35m"
-echo -e "\e[1;35m*\e[0m \e[1;31mT\e[1;32mE\e[1;33mL\e[1;34mE\e[1;35mG\e[1;36mR\e[1;37mA\e[1;31mM\e[0m : \e[4;34mKOLANDJS\e[0m         \e[1;35m"
-echo -e "\e[1;35m*\e[0m \e[1;31mG\e[1;32mI\e[1;33mT\e[1;34mH\e[1;35mU\e[1;36mB\e[0m : \e[4;34mhttps://github.com/Kolandone\e[0m \e[1;35m"
-echo -e "\e[1;35m*****************************************"
+clear
+if command -v figlet &>/dev/null; then
+    figlet -f slant "KOLAND"
+fi
+echo -e "${cyan}*****************************************${reset}"
+echo -e "${cyan}*${reset} ${red}Y${green}O${yellow}U${purple}T${cyan}U${green}B${white}E${reset} : ${purple}KOLANDONE${reset}         ${cyan}"
+echo -e "${cyan}*${reset} ${red}T${green}E${yellow}L${purple}E${cyan}G${green}R${white}A${red}M${reset} : ${purple}KOLANDJS${reset}         ${cyan}"
+echo -e "${cyan}*${reset} ${red}G${green}I${yellow}T${purple}H${cyan}U${green}B${reset} : ${purple}https://github.com/Kolandone${reset} ${cyan}"
+echo -e "${cyan}*****************************************${reset}"
+echo -e "${cyan}* ${green}Date:${reset} $(date '+%Y-%m-%d %H:%M:%S') ${cyan}*${reset}"
 echo ""
 
-echo -e "\e[1;36m*****************************************"
-echo -e "\e[1;32mPlease choose an option:(Type 99 to install selector)\e[0m"
-echo -e "\e[1;36m1. \e[1;33mIPv4 scan\e[0m"
-echo -e "\e[1;36m2. \e[1;33mIPv6 scan\e[0m"
-echo -e "\e[1;36m3. \e[1;33mV2ray and MahsaNG wireguard config\e[0m"
-echo -e "\e[1;36m4. \e[1;33mHiddify config for 1.4.0 - 1.9.0 versions\e[0m"
-echo -e "\e[1;36m5. \e[1;33mWarp License Cloner\e[0m"
-echo -e "\e[1;36m6. \e[1;33mHiddify config for 2.0 version or higher\e[0m"
-echo -e "\e[1;36m7. \e[1;33mInstall Worker Creator\e[0m"
-echo -e "\e[1;36m8. \e[1;33mRun Worker Creator (install it first)\e[0m"
-echo -e "\e[1;36m9. \e[1;33m Free subscription link(Soroush Mirzaei)\e[0m"
-echo -e "\e[1;36m10. \e[1;33mwireguard config for Hiddify and v2ray\e[0m"
-echo -e "\e[1;36m11. \e[1;33mCLEAN IP scanner\e[0m"
-echo -e "\e[1;36m12. \e[1;33mfastly CLEAN IP scanner\e[0m"
-echo -e "\e[1;36m13. \e[1;33mgcore CLEAN IP scanner\e[0m"
-echo -e "\e[1;36m14. \e[1;33mTelegram Proxy\e[0m"
-echo -e "\e[1;36m15. \e[1;33mSingBox installer(only for serv00)\e[0m"
-echo -e "\e[1;36m16. \e[1;33mUsque installer\e[0m"
-echo -e "\e[1;36m*****************************************"
-echo -en "\e[1;32mEnter your choice:\e[0m"
+echo -e "${cyan}+----+---------------------------------------------+${reset}"
+echo -e "${green}| No | Option                                      |${reset}"
+echo -e "${cyan}+----+---------------------------------------------+${reset}"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "1" "IPv4 scan"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "2" "IPv6 scan"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "3" "V2ray and MahsaNG wireguard config"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "4" "Hiddify config for 1.4.0 - 1.9.0 versions"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "5" "Warp License Cloner"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "6" "Hiddify config for 2.0 version or higher"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "7" "Install Worker Creator"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "8" "Run Worker Creator (install it first)"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "9" "Free subscription link (Soroush Mirzaei)"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "10" "Wireguard config for Hiddify and v2ray"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "11" "CLEAN IP scanner"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "12" "Fastly CLEAN IP scanner"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "13" "Gcore CLEAN IP scanner"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "14" "Telegram Proxy"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "15" "SingBox installer (only for serv00)"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "16" "Usque installer"
+printf "${cyan}| ${yellow}%-2s ${cyan}| ${yellow}%-43s ${cyan}|\n" "99" "Install Selector"
+echo -e "${cyan}+----+---------------------------------------------+${reset}"
+echo -en "${green}Enter your choice: ${reset}"
 read -r user_input
 
 
@@ -53,7 +67,6 @@ measure_latency6() {
     printf "| %-45s | %-10s |\n" "$ip_port" "$latency"
 }
 
-
 display_table_ipv4() {
     printf "+-----------------------+------------+\n"
     printf "| IP:Port               | Latency(ms) |\n"
@@ -69,7 +82,9 @@ display_table_ipv6() {
     echo "$1" | head -n 10 | while read -r ip_port; do measure_latency6 "$ip_port"; done
     printf "+---------------------------------------------+------------+\n"
 }
+
 cloner() {
+    
     if ! command -v wg &>/dev/null; then
         if [ -d "$PREFIX" ] && [ "$(uname -o)" = "Android" ]; then
             echo "Installing wireguard-tools"
@@ -118,22 +133,20 @@ cloner() {
         "21Ig0LE8-47emp59P-N190Vf5z"
     )
 
-    echo -e "${cyan}######################${rest}"
-    echo -en "${green}Enter a license (${yellow}Press Enter to use a random license, may not work${green}): ${rest}"
+    echo -e "${cyan}######################${reset}"
+    echo -en "${green}Enter a license (${yellow}Press Enter to use a random license, may not work${green}): ${reset}"
     read -r input_license
 
     if [ -z "$input_license" ]; then
-        # Choose a random license from the list
         license=$(shuf -n 1 -e "${licenses[@]}")
     else
         license="$input_license"
     fi
-    echo -e "${cyan}######################${rest}"
-    echo -e "${purple} Warp License cloner ${rest}"
-    echo -e "${green}Starting...${rest}"
-    echo -e "${purple}-------------------------------------${rest}"
+    echo -e "${cyan}######################${reset}"
+    echo -e "${purple}🔑 Warp License cloner 🔑${reset}"
+    echo -e "${green}Starting...${reset}"
+    echo -e "${purple}-------------------------------------${reset}"
     while true; do
-        # Requirements
         if command -v wg &>/dev/null; then
             private_key=$(wg genkey)
             public_key=$(wg pubkey <<<"$private_key")
@@ -146,7 +159,6 @@ cloner() {
         fcm_token="${install_id}:APA91b$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 134)"
         rand=$(tr -dc '0-9' </dev/urandom | head -c 3)
 
-        # Register
         response=$(curl --request POST "https://api.cloudflareclient.com/v0a${rand}/reg" \
             --silent \
             --location \
@@ -165,8 +177,6 @@ cloner() {
             }')
 
         echo "$response" | jq . >warp-config.json
-        # ___________________________________________
-        # Change License
         id=$(jq -r '.id' <warp-config.json)
         token=$(jq -r '.token' <warp-config.json)
 
@@ -181,8 +191,6 @@ cloner() {
                 "license": "'"$license"'"
             }')
 
-        # ___________________________________________
-        # Patch Account
         patch_one_response=$(curl -X PATCH "https://api.cloudflareclient.com/v0a${rand}/reg/${id}/account" \
             --silent \
             --location \
@@ -192,8 +200,6 @@ cloner() {
             --header "Authorization: Bearer ${token}" \
             --data '{"active": true}')
 
-        # ___________________________________________
-        # Get Data
         get_response=$(curl -X GET "https://api.cloudflareclient.com/v0a${rand}/reg/${id}" \
             --silent \
             --header "Authorization: Bearer ${token}" \
@@ -207,8 +213,6 @@ cloner() {
         balance=$(echo "$get_response" | jq -r '.account.quota')
         quota=$((balance / 1000000000))
 
-        # ___________________________________________
-        # Change License Again
         license=$(jq -r '.account.license' <warp-config.json)
 
         response=$(curl --request PUT "https://api.cloudflareclient.com/v0a${rand}/reg/${id}/account" \
@@ -222,8 +226,6 @@ cloner() {
                 "license": "'"$license"'"
             }')
 
-        # ___________________________________________
-        # Patch Account Again
         patch_two_response=$(curl -X PATCH "https://api.cloudflareclient.com/v0a${rand}/reg/${id}/account" \
             --silent \
             --location \
@@ -235,13 +237,11 @@ cloner() {
 
         if [ "$(echo "$patch_two_response" | jq '.result')" != "null" ]; then
             license=$(jq -r '.account.license' <warp-config.json)
-            echo -e "${green}$license ${rest}| ${cyan}$quota${rest}"
-            echo -e "${purple}-------------------------------------${rest}"
+            echo -e "${green}$license ${reset}| ${cyan}$quota${reset}"
+            echo -e "${purple}-------------------------------------${reset}"
             echo "$license | $quota" >>output.txt
         fi
 
-        # ___________________________________________
-        # Delete Account
         response=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE "https://api.cloudflareclient.com/v0a${rand}/reg/${id}" \
             --header "Authorization: Bearer ${token}" \
             --header "Accept: application/json" \
@@ -258,7 +258,6 @@ cloner() {
 }
 
 
-# Execute the chosen option
 if [ "$user_input" -eq 1 ]; then
     echo "Fetching IPv4 addresses from install.sh..."
     ip_list=$(echo "1" | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh) | grep -oP '(\d{1,3}\.){3}\d{1,3}:\d+')
@@ -299,11 +298,11 @@ elif [ "$user_input" -eq 14 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/quick-sub/refs/heads/main/proxy.sh)
 elif [ "$user_input" -eq 15 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/serv00/main/koland-serv00.sh)
-    elif [ "$user_input" -eq 16 ]; then
+elif [ "$user_input" -eq 16 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/Usque-Installer/refs/heads/main/usque.sh)
 elif [ "$user_input" -eq 99 ]; then
     bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/Selector/main/install.sh)
-    echo -e "\e[1;32mAfter this, you can run the Selector with \e[1;36mkl \e[1;32mcommand\e[0m"
-    else 
-    echo "Invalid input. Please enter between 1 and 16"
+    echo -e "${green}After this, you can run the Selector with ${cyan}kl ${green}command${reset}"
+else 
+    echo "Invalid input. Please enter between 1 and 16 or 99"
 fi
